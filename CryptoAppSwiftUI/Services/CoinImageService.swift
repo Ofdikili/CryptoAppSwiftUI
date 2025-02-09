8c88bf2d9c667afbfd9ef4d88dfe01ba6caf5f54
Ofdikili <omerfarukdikili00@gmail.com>
2025-02-09 23:18:55 +0300

    feat: Implement CoinImageService and NetworkingManager for image fetching
    - Created `CoinImageService` to handle downloading and caching of coin images.
    - Implemented `NetworkingManager` with a reusable `download` function for network requests.
    - Added `handleURLResponse` method to validate HTTP responses and ensure successful data retrieval.
    - Integrated Combine framework to manage asynchronous operations efficiently.
    - Commented out an unused `dataTaskPublisher` block that includes JSON decoding for potential future use.

---
+++ CryptoAppSwiftUI/Services/CoinImageService.swift
@@ -0,0 +1,36 @@
+//
+//  CoinImageService.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Ömer Faruk Dikili on 9.02.2025.
+//
+
+import Foundation
+import UIKit
+import Combine
+
+class CoinImageService {
+    @Published var image :UIImage? = nil
+    
+    var imageSubsctiption: AnyCancellable?
+
+    
+    init(urlString:String){
+        getCoinImage(urlString:urlString)
+    }
+    
+    private func getCoinImage(urlString:String){
+        guard let url = URL(string: urlString) else {
+            return ;
+        }
+        
+        imageSubsctiption = NetworkingManager.download(url: url)
+            .tryMap({
+                data->UIImage? in return UIImage(data: data)
+            }).sink(receiveCompletion: NetworkingManager.handleCompletion,
+            receiveValue:{ [weak self] image in
+                self?.image = image
+                self?.imageSubsctiption?.cancel()
+            })
+    }
+}
