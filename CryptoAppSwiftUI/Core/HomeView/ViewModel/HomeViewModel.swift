43ab883b3a37c04bef6f37a5799b8eedad7b2b4c
Ofdikili <omerfarukdikili00@gmail.com>
2025-02-18 15:35:04 +0300

    feat: Implement search functionality for filtering coins in HomeViewModel
    - Added `searchText` property to track user input.
    - Subscribed to `searchText` and `coinDataService.$allCoins` to filter coins dynamically.
    - Implemented `filterCoin` method to filter coins based on name, symbol, or ID.
    - Applied debounce to optimize performance and avoid excessive filtering.

--- CryptoAppSwiftUI/Core/HomeView/ViewModel/HomeViewModel.swift
+++ CryptoAppSwiftUI/Core/HomeView/ViewModel/HomeViewModel.swift
@@ -12,14 +12,7 @@ final class HomeViewModel: ObservableObject {
     @Published var showPortfolio: Bool = false
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
-    @Published var searchText: String = ""{
-        didSet {
-            if(!searchText.isEmpty){
-                self.allCoins = self.allCoins.filter { $0.name.lowercased().contains(searchText.lowercased()) }
-            }
-            
-        }
-    }
+    @Published var searchText: String = ""
     
     private let coinDataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
@@ -32,11 +25,43 @@ final class HomeViewModel: ObservableObject {
     }
     
     private func addSubscribers() {
-        coinDataService.$allCoins
+//        coinDataService.$allCoins
+//            .sink { [weak self] (coins) in
+//                self?.allCoins = coins
+//                }
+//                .store(in: &cancellables)
+        $searchText.combineLatest(coinDataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main) 
+            .map(filterCoin)
             .sink { [weak self] (coins) in
                 self?.allCoins = coins
                 }
                 .store(in: &cancellables)
             }
+    
+    
+    private func filterCoin(text:String , coins: [CoinModel])-> [CoinModel]{
+        guard !text.isEmpty else {
+            return coins
+        }
+        var lowercasedText = text.lowercased()
+        return coins.filter{ (coin)-> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||                                   coin.symbol.lowercased().contains(lowercasedText)  ||
+                   coin.id.lowercased().contains(lowercasedText)
+        }
+        
+//        .map { (text, startingCoins) -> [CoinModel] in
+//                guard !text.isEmpty else {
+//                    return startingCoins
+//                }
+//                let lowercasedText = text.lowercased()
+//                return startingCoins.filter { coin in
+//                    coin.name.lowercased().contains(lowercasedText) ||
+//                    coin.symbol.lowercased().contains(lowercasedText) ||
+//                    coin.id.lowercased().contains(lowercasedText)
+//                }
+//            }
+    }
+   
     }
 
