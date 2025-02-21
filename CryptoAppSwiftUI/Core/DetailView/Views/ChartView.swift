3cf99c59564112e4a264dac3f4dd2feec099389b
Ofdikili <omerfarukdikili00@gmail.com>
2025-02-21 18:36:01 +0300

    feat: add chart view and detail view model for crypto app
    - Implement `ChartView` to display a line chart for crypto price trends over 7 days
    - Animate the chart rendering with a percentage-based trim effect
    - Apply dynamic coloring to the chart based on price changes
    - Fetch and display coin details including statistics, description, and links in `DetailViewModel`
    - Use `Combine` to manage data updates from `CoinDetailDataService`
    - Structure overview and additional statistics with computed properties

---
+++ CryptoAppSwiftUI/Core/DetailView/Views/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Ömer Faruk Dikili on 21.02.2025.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
---
+++ CryptoAppSwiftUI/Extension/Date.swift
@@ -0,0 +1,8 @@
+//
+//  Date.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Ömer Faruk Dikili on 21.02.2025.
+//
+
+import Foundation
