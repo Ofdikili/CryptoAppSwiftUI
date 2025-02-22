4b0651b76836bc8e99c5bce1e6945aa8595f3049
Ofdikili <omerfarukdikili00@gmail.com>
2025-02-22 23:31:40 +0300

    feat(splash): Add native splash screen in SwiftUI
    - Implemented native splash screen for iOS using SwiftUI
    - Configured LaunchScreen.storyboard for initial display
    - Adjusted Info.plist settings for splash behavior
    - Ensured smooth transition to main app screen

---
+++ CryptoAppSwiftUI/Core/Settings/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Ömer Faruk Dikili on 22.02.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
